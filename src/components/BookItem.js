6bf3c264bb3f18cf23b6c501a49ec40a427dd21f
vanweerden <ando.vanweerden@gmail.com>
2020-06-08 06:39:28 +0800

    Added DeleteButton that shows 'x' on hover (over BookItem)

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,8 +1,12 @@
 // Present individual books as table rows
-import React from 'react';
-import { parseDate } from '../utils/dateFunctions'
+import React, { useState } from 'react';
+import { parseDate } from '../utils/dateFunctions';
+import { DeleteButton } from './DeleteButton';
 
 export const BookItem = (props) => {
+  // React Hook
+  const [isShown, setIsShown] = useState(false);
+
   const title = props.book.title;
   const authorFirst = props.book.authorFirstName;
   const authorLast = props.book.authorLastName;
@@ -10,15 +14,19 @@ export const BookItem = (props) => {
   const pages = props.book.pages;
   const language = props.book.language.charAt(0).toUpperCase() + props.book.language.slice(1);
   const blurb = props.book.blurb;
+  const id = props.book.id;
 
   return (
-    <div className='table-row book-item'>
+    <div  className='table-row book-item'
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}>
       <div className='table-cell title'>{title}</div>
       <div className='table-cell author'>{authorFirst} {authorLast}</div>
       <div className='table-cell date'>{finished}</div>
       <div className='table-cell pages'>{pages}</div>
       <div className='table-cell language'>{language}</div>
       <div className='table-cell blurb'>{blurb}</div>
+      <DeleteButton id={id} content={isShown && 'x'}/>
     </div>
   );
 }
---
+++ src/components/DeleteButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DeleteButton = (props) => {
+  return (
+    <div className='delete-button'>
+      {props.content}
+    </div>
+  );
+}
