e5207bf32eb21778e470e7dac117c479b1e7a66a
vanweerden <ando.vanweerden@gmail.com>
2020-06-10 06:38:22 +0800

    Minor tweak

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -4,7 +4,7 @@ export const DeleteButton = (props) => {
   const handleClick = () => console.log("Click!");
   return (
     <div  className='delete-button'
-          onClick={e => {
+          onClick={() => {
             window.confirm('Are you sure you want to delete this item?') &&
               props.deleteMethod(props.id)
           }}
