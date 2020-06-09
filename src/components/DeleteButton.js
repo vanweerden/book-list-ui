16ed6615669649cd796e1e20280567de93cbde81
vanweerden <ando.vanweerden@gmail.com>
2020-06-10 06:35:46 +0800

    Added confirm delete pop-up

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -4,7 +4,10 @@ export const DeleteButton = (props) => {
   const handleClick = () => console.log("Click!");
   return (
     <div  className='delete-button'
-          onClick={() => props.deleteMethod(props.id)}
+          onClick={e => {
+            window.confirm('Are you sure you want to delete this item?') &&
+              props.deleteMethod(props.id)
+          }}
     >
       {props.content}
     </div>
