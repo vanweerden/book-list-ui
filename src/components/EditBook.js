5fc941f75902fa1279873283c2645f1a5801ac9a
vanweerden <ando.vanweerden@gmail.com>
2020-07-17 09:54:06 +0800

    State now updates on input

--- src/components/EditBook.js
+++ src/components/EditBook.js
@@ -3,54 +3,77 @@ import React, { Component } from 'react';
 import { parseName } from '../utils/parseName';
 import { trimDate } from '../utils/dateFunctions';
 
-export const EditBook = props => {
-  return (
-    <div className='table-row book-item'>
-      <form method="update">
+export class EditBook extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      author: this.props.author,
+      finished: this.props.finished,
+      pages: this.props.pages,
+      language: this.props.language,
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
 
-        <div className='table-row form-row'>
-          <div className="table-cell form-cell form-title">
-            <input  type="text"
-                    name="title"
-                    maxLength="50"
-                    value={props.title}
-                    required />
-          </div>
+  handleInput(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
 
-          <div className="table-cell form-cell form-author">
-            <input  type="text"
-                    name="author"
-                    maxLength="40"
-                    value={props.author}
-                    />
-          </div>
+  render() {
+    return (
+      <div className='table-row book-item'>
+        <form method="update"
+              // TODO
+              // onSubmit={}
+              id="edit-book-form"
+              noValidate>
 
-          <div className="table-cell form-cell form-finished">
-            <input  type="date"
-                    name="finished"
-                    value={trimDate(props.finished)}
-                    required/>
-          </div>
+          <div className='table-row form-row'>
+            <div className="table-cell form-cell form-title">
+              <input  type="text"
+                      name="title"
+                      maxLength="50"
+                      defaultValue={this.state.title}
+                      onChange={this.handleInput}
+                      required />
+            </div>
 
-          <div className="table-cell form-cell form-pages">
-            <input  type="text"
-                    name="pages"
-                    value={props.pages}
-                    maxLength="4"/>
-          </div>
+            <div className="table-cell form-cell form-author">
+              <input  type="text"
+                      name="author"
+                      maxLength="40"
+                      defaultValue={this.state.author}
+                      />
+            </div>
 
-          <div className="table-cell form-cell form-language">
-            <select name="language"
-                    className="clickable">
-              <option value="english">English</option>
-              <option value="japanese">Japanese</option>
-              <option value="french">French</option>
-              <option value="latin">Latin</option>
-            </select>
-            <div className='error'></div>
+            <div className="table-cell form-cell form-finished">
+              <input  type="date"
+                      name="finished"
+                      defaultValue={trimDate(this.state.finished)}
+                      required/>
+            </div>
+
+            <div className="table-cell form-cell form-pages">
+              <input  type="text"
+                      name="pages"
+                      defaultValue={this.state.pages}
+                      maxLength="4"/>
+            </div>
+
+            <div className="table-cell form-cell form-language">
+              <select name="language"
+                      className="clickable">
+                <option value="english">English</option>
+                <option value="japanese">Japanese</option>
+                <option value="french">French</option>
+                <option value="latin">Latin</option>
+              </select>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
-  )
+        </form>
+      </div>
+    )
+  }
 }
